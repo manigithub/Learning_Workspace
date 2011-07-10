ef5d6612bc97d8e4045a15e9f3798eea56ad9411
Mani V <000000mani@gmail.com>
2011-07-11 01:33:25 +0800

    WPF Learning - 1st session

---
+++ 2011_Learnings/WPF_Samples/1_Fundamentals/1_Fundamentals/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//Add the below:
+using System.Windows;
+
+namespace _1_Fundamentals
+{
+    class Program
+    {
+        [STAThread]//Without this Invalid Operation Exception for any WPF program
+        static void Main(string[] args)
+        {
+            
+            Window winHelloWorld = new Window();
+            winHelloWorld.Title = "Hello, world";
+            winHelloWorld.Content = "I am the Hello world screen";
+            //winHelloWorld.Show();
+
+            Application app = new Application();
+            Application app2 = new Application();
+            app.Run(winHelloWorld);
+            app2.Run(winHelloWorld);
+        }
+    }
+}
